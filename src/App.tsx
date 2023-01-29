77ed63e46b6a8d98f8c363031e3fec17bf330946
TanasieAlexandru <alexandru_tanasie@yahoo.com>
2023-01-29 18:12:54 +0200

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import TicTacToeGame from 'components/TicTacToe/TicTacToe';
 import Home from 'components/Home/Home';
 
 export default function App() {
 	return (
-		<div className="App">
-			<Home />
-			<TicTacToeGame />
-		</div>
+		<Router>
+			<div className="App">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/ticTacToeGame">TicTacToeGame</Link>
+					</li>
+				</ul>
+				<Routes>
+					<Route path="/" element={<Home />}></Route>
+					<Route path="/ticTacToeGame" element={<TicTacToeGame />}></Route>
+				</Routes>
+			</div>
+		</Router>
 	);
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Home() {
-	return <h1>Welcome to the world of Geeks!</h1>;
+	return <h1>Welcome home tva!</h1>;
 }
